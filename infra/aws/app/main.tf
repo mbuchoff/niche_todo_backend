9b14694c44f8848af52193e7215f7d9e8f7df3ec
michaelbParcelshield <michaelb@parcelshield.com>
2026-02-02 05:25:18 -0800

    Allow CI to skip AWS profile

--- infra/aws/app/main.tf
+++ infra/aws/app/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 provider "aws" {
   region  = var.aws_region
-  profile = var.aws_profile
+  profile = var.aws_profile != "" ? var.aws_profile : null
 }
 
 data "aws_availability_zones" "available" {
